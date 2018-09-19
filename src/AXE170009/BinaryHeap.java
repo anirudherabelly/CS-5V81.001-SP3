2ed2d417c654d671a749baed95a42d5a6bdba579
anirudherabelly <anirudh.erabelly@gmail.com>
2018-09-19 17:04:19 -0500

    added method and few other changes

--- src/AXE170009/BinaryHeap.java
+++ src/AXE170009/BinaryHeap.java
@@ -9,10 +9,10 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     Comparator<T> comp;
     private int size;
     private int capacity;
+    
     // Constructor for building an empty priority queue using natural ordering of T
-
     public BinaryHeap(T[] q) {
-	// Use a lambda expression to create comparator from compareTo
+    	// Use a lambda expression to create comparator from compareTo
 	    this(q, (T a, T b) -> a.compareTo(b));
     }
 
@@ -36,19 +36,19 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     }
 
     public void add(T x) { /* throw exception if pq is full */
-        boolean inserted = offer(x);
-        if(!inserted){
-            throw new UnsupportedOperationException("Queue full");
+        boolean isInserted = offer(x);
+        if(!isInserted){
+            throw new UnsupportedOperationException("Queue is full");
         }
     }
 
     public boolean offer(T x) { /* return false if pq is full */
-        if(size==capacity){
+        if(size == capacity){
             return false;
         }
         pq[size] = x;
         percolateUp(size);
-        ++size;
+        size++;
         return true;
     }
 
@@ -61,95 +61,112 @@ public class BinaryHeap<T extends Comparable<? super T>> {
             throw new UnsupportedOperationException("Queue is empty");
         }
         T returnVal = pq[0];
-        if(size>1) {
+        if(size > 1) {
             pq[0] = pq[size - 1];
             percolateDown(0);
         }
-        --size;
+        size--;
 	    return returnVal;
     }
 
-    /**
-        * return null if pq is empty
-     */
-    public T peek() {
-        return size==0?null:pq[0];
+    public T peek() { /* return null if pq is empty */
+        return size == 0 ? null : pq[0];
     }
 
     /** pq[i] may violate heap order with parent */
-    void percolateUp(int i) { /* to be implemented */
-        int parent = parent(i);
-        T temp = pq[i];
-        while(i!=0&&comp.compare(pq[parent],temp)>0){
-            pq[i] = pq[parent];
-            i=parent;
-            parent = parent(i);
+    void percolateUp(int index) { 
+    	int parentIndex = parent(index);
+        T temp = pq[index];
+        while(index != 0 && comp.compare(pq[parentIndex], temp) > 0){
+            pq[index] = pq[parentIndex];
+            index = parentIndex;
+            parentIndex = parent(index);
         }
-        pq[i] = temp;
+        pq[index] = temp;
     }
 
     /** pq[i] may violate heap order with children */
-    void percolateDown(int i) {
-        int leftChild = leftChild(i);
-        while(true){
-
-            if()
+    void percolateDown(int index) {
+        int leftChildIndex = leftChild(index);
+        int rightChildIndex = rightChild(index);
+        int temp = index;
+        
+        if(leftChildIndex != -1 && comp.compare(this.pq[index], this.pq[leftChildIndex]) > 0) {
+        	temp = leftChildIndex;
         }
-
+        if(rightChildIndex != -1 && comp.compare(this.pq[temp], this.pq[rightChildIndex]) > 0) {
+        	temp = rightChildIndex;
+        }
+        
+        if(temp != index) {
+        	swap(this.pq[temp], this.pq[index]);
+        	percolateDown(temp);
+        }
     }
-
+    
     // Assign x to pq[i].  Indexed heap will override this method
-    void move(int i, T x) {
-	    pq[i] = x;
+    void move(int index, T x) {
+	    pq[index] = x;
     }
 
-    int parent(int i) {
-	    return (i-1)/2;
+    int parent(int index) {
+	    return (index - 1) / 2;
     }
 
-    int leftChild(int i) {
-	    return 2*i + 1;
+    int leftChild(int index) {
+	    return 2 * index + 1;
     }
+    
+    int rightChild(int index) {
+    	return 2 * index + 2;
+    }
+    
+    private  void swap(T a, T b) {
+    	T temp = a;
+    	a = b;
+    	b = temp;
+    }
+    
+    // end of functions for team project
 
-// end of functions for team project
 
 
-
-
-// start of optional problem: heap sort (Q2)
+    // start of optional problem: heap sort (Q2)
 
     /** Create a heap.  Precondition: none. 
-     *  Implement this ifsolving optional problem
+     *  Implement this if solving optional problem
      */
     void buildHeap() {
     }
 
-    /* sort array arr[].
-       Sorted order depends on comparator used to buid heap.
-       min heap ==> descending order
-       max heap ==> ascending order
-       Implement this for optional problem
+    /** sort array arr[].
+     * Sorted order depends on comparator used to buid heap.
+     * min heap ==> descending order
+     * max heap ==> ascending order
+     * Implement this for optional problem
      */
     public static<T> void heapSort(T[] arr, Comparator<T> c) { /* to be implemented */
 
     }
 
-    // Sort array using natural ordering
+    /** Sort array using natural ordering */
     public static<T extends Comparable<? super T>> void heapSort(T[] arr) {
 	    heapSort(arr, (T a, T b) -> a.compareTo(b));
     }
+    
     // end of optional problem: heap sort (Q2)
 
 
 
     // start of optional problem: kth largest element (Q3)
-
+    
+    /** TO DO.  Replaces root of binary heap by x if x has higher priority
+     * (smaller) than root, and restore heap order.  Otherwise do nothing. 
+     * This operation is used in finding largest k elements in a stream.
+     * Implement this if solving optional problem.
+     */
     public void replace(T x) {
-	/* TO DO.  Replaces root of binary heap by x if x has higher priority
-	     (smaller) than root, and restore heap order.  Otherwise do nothing. 
-	   This operation is used in finding largest k elements in a stream.
-	   Implement this if solving optional problem.
-	 */
+		
     }
 
     /** Return the kth largest element of stream using custom comparator.
@@ -165,8 +182,9 @@ public class BinaryHeap<T extends Comparable<? super T>> {
      *  If stream has less than k elements return null.
      */
     public static<T extends Comparable<? super T>> T kthLargest(Iterator<T> stream, int k) {
-	return kthLargest(stream, k, (T a, T b) -> a.compareTo(b));
+    	return kthLargest(stream, k, (T a, T b) -> a.compareTo(b));
     }
-// end of optional problem: kth largest element (Q3)
+    
+    // end of optional problem: kth largest element (Q3)
 
 }
