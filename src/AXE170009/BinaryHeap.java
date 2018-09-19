658b00bbfbe8d79b1e1ac5f317ee710017d9ebfa
kautilreddy <KAU14@MacBook-Pro.local>
2018-09-19 15:21:56 -0500

    added offer,add methods

--- src/AXE170009/BinaryHeap.java
+++ src/AXE170009/BinaryHeap.java
@@ -7,17 +7,21 @@ import java.util.Iterator;
 public class BinaryHeap<T extends Comparable<? super T>> {
     T[] pq;
     Comparator<T> comp;
+    private int size;
+    private int capacity;
     // Constructor for building an empty priority queue using natural ordering of T
 
     public BinaryHeap(T[] q) {
 	// Use a lambda expression to create comparator from compareTo
 	    this(q, (T a, T b) -> a.compareTo(b));
     }
-    
+
     // Constructor for building an empty priority queue with custom comparator
     public BinaryHeap(T[] q, Comparator<T> c) {
 	    pq = q;
 	    comp = c;
+	    capacity = q.length;
+	    size = 0;
     }
     
     /** Build a priority queue with a given array q, using q[0..n-1].
@@ -32,43 +36,79 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     }
 
     public void add(T x) { /* throw exception if pq is full */
+        boolean inserted = offer(x);
+        if(!inserted){
+            throw new UnsupportedOperationException("Queue full");
+        }
     }
 
     public boolean offer(T x) { /* return false if pq is full */
-	    return false;
+        if(size==capacity){
+            return false;
+        }
+        pq[size] = x;
+        percolateUp(size);
+        ++size;
+        return true;
     }
 
     public T remove() { /* throw exception if pq is empty */
-	    return null;
+	    return poll();
     }
 
-    public T poll() { /* return false if pq is empty */
-	    return null;
+    public T poll() { /* return null if pq is empty */
+        if(size==0){
+            throw new UnsupportedOperationException("Queue is empty");
+        }
+        T returnVal = pq[0];
+        if(size>1) {
+            pq[0] = pq[size - 1];
+            percolateDown(0);
+        }
+        --size;
+	    return returnVal;
     }
 
-    public T peek() { /* return null if pq is empty */
-	    return null;
+    /**
+        * return null if pq is empty
+     */
+    public T peek() {
+        return size==0?null:pq[0];
     }
 
     /** pq[i] may violate heap order with parent */
     void percolateUp(int i) { /* to be implemented */
+        int parent = parent(i);
+        T temp = pq[i];
+        while(i!=0&&comp.compare(pq[parent],temp)>0){
+            pq[i] = pq[parent];
+            i=parent;
+            parent = parent(i);
+        }
+        pq[i] = temp;
     }
 
     /** pq[i] may violate heap order with children */
-    void percolateDown(int i) { /* to be implemented */
+    void percolateDown(int i) {
+        int leftChild = leftChild(i);
+        while(true){
+
+            if()
+        }
+
     }
 
     // Assign x to pq[i].  Indexed heap will override this method
     void move(int i, T x) {
-	pq[i] = x;
+	    pq[i] = x;
     }
 
     int parent(int i) {
-	return (i-1)/2;
+	    return (i-1)/2;
     }
 
     int leftChild(int i) {
-	return 2*i + 1;
+	    return 2*i + 1;
     }
 
 // end of functions for team project
--- src/AXE170009/TestMain.java
+++ src/AXE170009/TestMain.java
@@ -1,6 +1,7 @@
 package AXE170009;
 //Created by Kautil & Anirudh on 08/25/2018.
 
+import java.lang.reflect.Type;
 import java.util.NoSuchElementException;
 
 /**
@@ -26,19 +27,37 @@ public class TestMain {
             System.out.println("-ea option enabled good to go");
         }
         TestMain tester = new TestMain();
+        tester.testAdd();
         System.out.println("All Tests passed");
+    }
+
+    private void testAdd() {
+        Integer[] q = new Integer[4];
+        BinaryHeap<Integer> heap = new BinaryHeap<>(q);
+        heap.add(0);
+        assert heap.peek()==0;
+        heap.add(5);
+        assert heap.peek()==0;
+        heap.add(2);
+        assert heap.peek()==0;
+        heap.add(-2);
+        assert heap.peek()==-2;
+        checkException(()->heap.add(1),UnsupportedOperationException.class);
+        boolean inserted = heap.offer(1);
+        assert !inserted;
     }
 
     /**
      * Runs the given function inside a try-catch block to capture and assert that NoSuchElementException is raised
      * @param function function that needs to be run inside the try block
+     * @param expectedEx the class type the function should throw
      */
-    private void checkNoSuchElementException(Runnable function){
+    private void checkException(Runnable function, Class<?> expectedEx){
         try {
             function.run();
             assert false;
         }catch (Exception e){
-            assert e instanceof NoSuchElementException;
+            assert e.getClass() == expectedEx;
         }
     }
 }
